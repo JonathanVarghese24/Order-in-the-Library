cfd9780515a79d1273f8e9e970f369f7082bd4f0
JonathanVarghese24 <JonathanVarghese24@gmail.com>
2025-02-24 09:54:33 -0600

    Initial Commit

---
+++ Order in the Library/Order_in_the_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Order_in_the_LibraryApp.swift
+//  Order in the Library
+//
+//  Created by JV on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Order_in_the_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Order in the LibraryTests/Order_in_the_LibraryTests.swift
@@ -0,0 +1,17 @@
+//
+//  Order_in_the_LibraryTests.swift
+//  Order in the LibraryTests
+//
+//  Created by JV on 2/24/25.
+//
+
+import Testing
+@testable import Order_in_the_Library
+
+struct Order_in_the_LibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
