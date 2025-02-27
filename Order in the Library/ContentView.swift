94a27c44aa4ce02cb319626c9ed54b94ebe7b6dc
Chase Hashiguchi <Chasehashigu@gmail.com>
2025-02-27 09:25:06 -0600

    made instructions view

--- Order in the Library/ContentView.swift
+++ Order in the Library/ContentView.swift
@@ -9,16 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+                VStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
+                    NavigationLink("Game Instructions", destination: InstructionsView())
+                }
+                .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct InstructionsView: View {
+    var body: some View {
+        ZStack {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Game Instructions")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                Text(" 1) sort books by there decimal number from least to greatest, if the book is fiction sort the book by the authors last name")
+                Text(" 2) once you have sorted the books click the check books button to see if they are correct")
+                Text(" 3) all books will be randomly selected so levels will not be the exact same ")
+                Text(" 4) if needed there will be a check alphabet button to see the alphabet")
+                Spacer()
+            }
+        }
+    }
 }
