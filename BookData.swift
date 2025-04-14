b94761b0778daad162e41fd7d7ce507cce261097
Chase Hashiguchi <Chasehashigu@gmail.com>
2025-04-14 09:31:53 -0500

    put book titles in comments

--- BookData.swift
+++ BookData.swift
@@ -8,29 +8,29 @@
 import Foundation
 
 let sampleBooks: [Books] = [
-    Books(imageName: "", authorLastName: "WAT", deweyDecimal: "001.3"),
-    Books(imageName: "", authorLastName: "MIT", deweyDecimal: "006.3"),
-    Books(imageName: "", authorLastName: "GAA", deweyDecimal: "100"),
-    Books(imageName: "", authorLastName: "BAG", deweyDecimal: "128.2"),
-    Books(imageName: "", authorLastName: "KAH", deweyDecimal: "150"),
-    Books(imageName: "", authorLastName: "SCH", deweyDecimal: "153.42"),
-    Books(imageName: "", authorLastName: "TOL", deweyDecimal: "158.1"),
-    Books(imageName: "", authorLastName: "PIN", deweyDecimal: "401"),
-    Books(imageName: "", authorLastName: "DEU", deweyDecimal: "415"),
-    Books(imageName: "", authorLastName: "ABB", deweyDecimal: "516.3"),
-    Books(imageName: "", authorLastName: "SAG", deweyDecimal: "520"),
-    Books(imageName: "", authorLastName: "TYS", deweyDecimal: "523.1"),
-    Books(imageName: "", authorLastName: "HAW", deweyDecimal: "530.11"),
-    Books(imageName: "", authorLastName: "MCP", deweyDecimal: "550"),
-    Books(imageName: "", authorLastName: "TRI", deweyDecimal: "613.25"),
-    Books(imageName: "", authorLastName: "JAM", deweyDecimal: "616.89"),
-    Books(imageName: "", authorLastName: "NEW", deweyDecimal: "650.1"),
-    Books(imageName: "", authorLastName: "BER", deweyDecimal: "700"),
-    Books(imageName: "", authorLastName: "SPI", deweyDecimal: "741.5"),
-    Books(imageName: "", authorLastName: "SON", deweyDecimal: "770.1"),
-    Books(imageName: "", authorLastName: "GOL", deweyDecimal: "791.43"),
-    Books(imageName: "", authorLastName: "SHA", deweyDecimal: "821.3"),
-    Books(imageName: "", authorLastName: "ORW", deweyDecimal: "823.912"),
-    Books(imageName: "", authorLastName: "WEI", deweyDecimal: "910.4"),
-    Books(imageName: "", authorLastName: "WIE", deweyDecimal: "940.5318")
+    Books(imageName: "", authorLastName: "WAT", deweyDecimal: "001.3"), //Everything Is Obvious
+    Books(imageName: "", authorLastName: "MIT", deweyDecimal: "006.3"), //Artificial Intelligence
+    Books(imageName: "", authorLastName: "GAA", deweyDecimal: "100"), //Sophie's World
+    Books(imageName: "", authorLastName: "BAG", deweyDecimal: "128.2"), //How to Think Like a Philosopher
+    Books(imageName: "", authorLastName: "KAH", deweyDecimal: "150"), //Thinking, Fast and Slow
+    Books(imageName: "", authorLastName: "SCH", deweyDecimal: "153.42"), //The Paradox of Choice
+    Books(imageName: "", authorLastName: "TOL", deweyDecimal: "158.1"), //The Power of Now
+    Books(imageName: "", authorLastName: "PIN", deweyDecimal: "401"), //The Language Instinct
+    Books(imageName: "", authorLastName: "DEU", deweyDecimal: "415"), // The Unfolding of Language
+    Books(imageName: "", authorLastName: "ABB", deweyDecimal: "516.3"), //Flatland
+    Books(imageName: "", authorLastName: "SAG", deweyDecimal: "520"), //Cosmos
+    Books(imageName: "", authorLastName: "TYS", deweyDecimal: "523.1"), //Astrophysics for People in a Hurry
+    Books(imageName: "", authorLastName: "HAW", deweyDecimal: "530.11"), //A Brief History of Time
+    Books(imageName: "", authorLastName: "MCP", deweyDecimal: "550"), //Annals of the Former World
+    Books(imageName: "", authorLastName: "TRI", deweyDecimal: "613.25"), //Intuitive Eating
+    Books(imageName: "", authorLastName: "JAM", deweyDecimal: "616.89"), //An Unquiet Mind
+    Books(imageName: "", authorLastName: "NEW", deweyDecimal: "650.1"),// Deep Work
+    Books(imageName: "", authorLastName: "BER", deweyDecimal: "700"),//Ways of Seeing
+    Books(imageName: "", authorLastName: "SPI", deweyDecimal: "741.5"),//Maus
+    Books(imageName: "", authorLastName: "SON", deweyDecimal: "770.1"),//On Photography
+    Books(imageName: "", authorLastName: "GOL", deweyDecimal: "791.43"),//Adventures in the Screen Trade
+    Books(imageName: "", authorLastName: "SHA", deweyDecimal: "821.3"),//Shakespeare's Sonnets
+    Books(imageName: "", authorLastName: "ORW", deweyDecimal: "823.912"),//1984
+    Books(imageName: "", authorLastName: "WEI", deweyDecimal: "910.4"),//The Geography of Bliss
+    Books(imageName: "", authorLastName: "WIE", deweyDecimal: "940.531")//Night
 ]
